4a416da657c6b768d7bdf9b4a8715486963f66d9
MikeChigev <mchigev@gmail.com>
2022-11-26 16:21:53 +0300

    task4

---
+++ Work/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу котора принимает на вход число и проверяет кратно ли оно одовременно 7 и 23
+// 14 - нет
+// 46 - нет
+// 161 - да
+
+Console.Clear();
+Console.WriteLine("Введите число:  ");
+int number = int.Parse(Console.ReadLine());
+ if (number % 7 == 0 & number % 23 == 0)
+ {
+    Console.WriteLine(" - да");
+    
+ }
+else 
+{
+    Console.WriteLine(" - нет ");
+}
