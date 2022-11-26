fc4cf708806546129b6f3b2436c02314b9e2334a
MikeChigev <mchigev@gmail.com>
2022-11-26 13:06:42 +0300

    Task1

---
+++ Work/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу которая выводит случайное число из отрезка [10_________90] и показывает наибольшию цифру числа
+//78 -> 8
+//12 -> 2
+//85 -> 8 
+
+Console.Clear();
+int randomDigit = new Random().Next(10, 100);
+Console.WriteLine("Случайное число " + randomDigit);
+
+int ostatok = randomDigit%10;
+Console.WriteLine("Последняя цифра случайного числа:  " + ostatok);
+
+int firstDigit = randomDigit/10;
+Console.WriteLine("Первая цифра случайного числа:   " + firstDigit);
+
+/*int maxDigit = 0;
+if (ostatok > firstDigit) 
+    maxDigit = ostatok;
+else maxDigit = firstDigit;
+
+Console.WriteLine("Наибольшая цифра числа:  " + maxDigit);
+*/
+ int maxDigit = firstDigit;
+
+ if (ostatok > firstDigit)
+     maxDigit = ostatok; 
+ 
+ else
+ Console.WriteLine("Наибольшая цифра числа:  " + maxDigit);
